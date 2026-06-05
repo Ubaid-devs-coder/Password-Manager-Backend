286a1ae2d2026683436693eeb6c57f3dad4fd0e0
unknown <ubaid11357@gmail.com>
2026-06-05 17:00:53 +0500

    added mongodb backend

--- server.js
+++ server.js
@@ -1,52 +1,51 @@
-const express = require('express')
 const dotenv = require('dotenv')
-const { MongoClient } = require('mongodb');
-const bodyparser = require('body-parser')
-const cors = require('cors')
-
 dotenv.config()
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const connect = require('./db')
+const User = require('./user.model')
 
-// Database Name
-const dbName = 'passop';
 const app = express()
 const port = 3000
+
 app.use(bodyparser.json())
 app.use(cors())
 
-client.connect();
+connect()
 
 // Get all passwords
 app.get('/', async (req, res) => {
-  const db = client.db(dbName);
-  const collection = db.collection('passwords');
-  const findResult = await collection.find({}).toArray();
-  res.json(findResult)
+  try {
+    const users = await User.find({})
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message })
+  }
 })
 
-// Save or add a password
+// Save a password
 app.post('/', async (req, res) => {
-  const password = req.body
-  const db = client.db(dbName);
-  const collection = db.collection('passwords');
-  const findResult = await collection.insertOne(password)
-  res.send({success: true, result: findResult})
-})
-
-// Delete a password by id
-app.delete('/', async (req, res) => {
-  const password = req.body
-  const db = client.db(dbName);
-  const collection = db.collection('passwords');
-  const findResult = await collection.deleteOne(password)
-  res.send({success: true, result: findResult})
+  try {
+    const user = new User(req.body)
+    const result = await user.save()
+    res.json({ success: true, result })
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message })
+  }
 })
 
 // Delete a password
+app.delete('/', async (req, res) => {
+  try {
+    const result = await User.deleteOne({ _id: req.body.id })
+    res.json({ success: true, result })
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message })
+  }
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port}`)
 })
