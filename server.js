04c0e0d50b5d06b4340d1c317d22e6bc006abef2
unknown <ubaid11357@gmail.com>
2026-06-06 18:28:07 +0500

    backend update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connect = require('./db')
 const User = require('./user.model')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyparser.json())
 app.use(cors())
